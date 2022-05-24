b48bca5c0941c11e28842d8e1942ef27e25bc024
decreo17 <decreogithub@gmail.com>
2022-05-24 09:17:22 +0800

    updated the puzzle based on the document
    - add comments/doc string
    - word: change to randomly pick a word at initiation
    - blank_puzzle: move from man
    - is_in_puzzle: move from man
    - man: add man instance since puzzle should be controlling the man, this means that we need to remove man from the director
    -  update get_word() remove the random selection of word, this was already move to initiation
    - move get_blank_puzzle() from man
    - move check_guess() from man
    - move is_winner() from man

--- jumper/game/puzzle.py
+++ jumper/game/puzzle.py
@@ -1,4 +1,6 @@
+from man import Man
 import random
+
 class Puzzle:
     """A word to be guessed. 
     
@@ -6,14 +8,60 @@ class Puzzle:
 
     Attributes:
         word_list (List(str)): list of possible words to guess.
-        word (str): the word to guess.
-        
+        word (str): randomly pick the word to guess from word_list.
+        blank_puzzle(list): The initial state as well as the main list that will be updated for each guess
+        _is_in_puzzle(boolean): every guess this will be update to return if the letter is in the word or not
+        man(Man): The instance of a Man
     """
 
     def __init__(self):
+        """Constructs a new Puzzle.
+        
+        Args:
+            self (Puzzle): an instance of Puzzle.
+        """
         self._word_list = ["mango", "house", "programming", "computer"]
-        self._word = ""
-
+        self._word = random.choice(self._word_list)
+        self._blank_puzzle = ["_"] * len(self._word)
+        self._is_in_puzzle = False
+        self._man = Man() #add the man in the puzzle and remove it from the director
+    
+    def get_blank_puzzle(self):
+        """Gets the puzzle hint(blank_puzzle).
+        
+        Args:
+            self (Puzzle): an instance of Puzzle.
+        """
+        return self._blank_puzzle
+          
     def get_word(self):
-       self._word = random.choice(self._word_list)
-       return self._word
+        """Gets the word.
+        
+        Args:
+            self (Puzzle): an instance of Puzzle.
+        """
+        #self._word = random.choice(self._word_list) #add this in the initiation instead so tha we could call get_word without changing the word
+        return self._word
+
+    def check_guess(self, letter):
+        """Check the guess of the player and update is_in_puzzle, blank_puzzle and man.fails when needed.
+        
+        Args:
+            self (Puzzle): an instance of Puzzle.
+            letter (str): input from user
+        """
+        self._is_in_puzzle = False
+        for i in range(len(self._word)):
+            if letter == self._word[i]:
+                self._blank_puzzle[i] = letter
+                self._is_in_puzzle = True
+        if self._is_in_puzzle != True:
+            self._man.fails += 1
+
+    def is_winner(self):
+        """Check if the underscores are no longer in the blank_puzzle, if true then the game is over.
+        
+        Args:
+            self (Puzzle): an instance of Puzzle.
+        """
+        return "_" not in self._blank_puzzle
